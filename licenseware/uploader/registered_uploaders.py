98e495a5d69ed5ca79955683dc6e1ac0040894c1
ClimenteA <climente.alin@gmail.com>
2022-09-20 16:38:12 +0300

    fix: cant use producer in a celery task

--- licenseware/uploader/registered_uploaders.py
+++ licenseware/uploader/registered_uploaders.py
@@ -172,7 +172,7 @@ class RegisteredUploaders:  # pragma no cover
         response = uploader.update_status_handler(
             tenant_id, uploader.uploader_id, status, self.config
         )
-        self.registry_updater.delay()
+        self.registry_updater()
         return response
 
     # PRIVATE
